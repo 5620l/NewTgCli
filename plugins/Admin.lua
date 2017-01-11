99be38d558f3cb35fa345b6606c51739710288de
p00ria <amir1300sobatdar@gmail.com>
2017-01-11 22:51:36 +0330

    Update Admin.lua

--- plugins/Admin.lua
+++ plugins/Admin.lua
@@ -4,37 +4,37 @@ if matches[1] == 'import' then
 tg.importChatInviteLink('https://telegram.me/joinchat/'..matches[2]) 
 elseif matches[1] == 'setbotname' then 
 if matches[3] then 
-text = "نام ربات به "..matches[2]..matches[3].." تغییر یافت." 
-tg.sendMessage(msg.chat_id_, 0, 1, text, 1, 'html') 
-tg.changeName(matches[2], matches[3]) 
+text = "<b>Bot Name Changed To "..matches[2]..matches[3].." </b>" 
+bot.sendMessage(msg.chat_id_, 0, 1, text, 1, 'html') 
+bot.changeName(matches[2], matches[3]) 
 else 
-text = "نام ربات به "..matches[2].." تغییر یافت." 
-tg.sendMessage(msg.chat_id_, 0, 1, text, 1, 'html') 
-tg.changeName(matches[2], '') 
+text = "<b>Bot Name Changed To "..matches[2].."</b>"
+bot.sendMessage(msg.chat_id_, 0, 1, text, 1, 'html') 
+bot.changeName(matches[2], '') 
 end 
 elseif matches[1] == 'setbotusername' then 
-tg.changeUsername(matches[2])
+bot.changeUsername(matches[2])
 elseif matches[1] == 'rembotusername' then 
-tg.changeUsername('') 
+bot.changeUsername('') 
 elseif matches[1] == 'setbotphoto' then 
-tg.sendMessage(msg.chat_id_, 0, 1, 'لطفا تصویر را ارسال کنید.', 1, 'html') 
+bot.sendMessage(msg.chat_id_, 0, 1, '<b>Now Send Photo!</b>', 1, 'html') 
 redis:set('botphoto','yes') 
 elseif matches[1] == 'photo' and redis:get('botphoto') then 
 local file = 'bot/bot.jpg' 
 print('File downloaded to:', msg.content_.photo_.sizes_[0].photo_.path_) 
 os.rename(msg.content_.photo_.sizes_[0].photo_.path_, file) 
 print('File moved to:', file) 
-tg.setProfilePhoto(msg.content_.photo_.sizes_[0].photo_.path_) 
+bot.setProfilePhoto(msg.content_.photo_.sizes_[0].photo_.path_) 
 redis:del('botphoto',true) 
 end 
 if matches[1] == 'markread' then 
 if matches[2] == 'on' then 
 text = '<b> Mark Read => on</b>'
-	tg.sendMessage(msg.chat_id_, 0, 1, text, 1, 'html') 
+	bot.sendMessage(msg.chat_id_, 0, 1, text, 1, 'html') 
 redis:set('markread','on') 
 elseif matches[2] == 'off' then 
 text = '<b> Mark Read => off</b>'
-	tg.sendMessage(msg.chat_id_, 0, 1, text, 1, 'html') 
+	bot.sendMessage(msg.chat_id_, 0, 1, text, 1, 'html') 
 redis:set('markread','off') 
 end 
 end 
