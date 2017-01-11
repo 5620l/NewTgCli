d064ac18d31ba0a781b0980ef1e48ac38bfb9471
p00ria <amir1300sobatdar@gmail.com>
2017-01-12 00:28:42 +0330

    Update

--- plugins/Pin.lua
+++ plugins/Pin.lua
@@ -1,10 +1,10 @@
 local function run(msg, matches)
         if matches[1] == 'pin' and is_owner(msg) then
             bot.pinChannelMessage(msg.chat_id_, msg.reply_to_message_id_, 1)
-                bot.sendMessage(msg.chat_id_, 0, 0, 'پیام سنجاق شد.', 0)
+                bot.sendMessage(msg.chat_id_, 0, 0, '<b>Message Has Been Pinned!</b>', 1, 'html')
         elseif matches[1] == 'unpin' and is_owner(msg) then
             bot.unpinChannelMessage(msg.chat_id_)
-                bot.sendMessage(msg.chat_id_, 0, 0, 'پیام از سنجاق برداشته شد.', 0)
+                bot.sendMessage(msg.chat_id_, 0, 0, '<b>Message Has Been Unpinned!</b>', 1, 'html')
         end end return {
   patterns = {
     "^[#!/](pin)$",
