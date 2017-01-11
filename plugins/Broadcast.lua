408d2c117f8110ea39eb0641de330a3e6d2847ad
p00ria <amir1300sobatdar@gmail.com>
2017-01-12 00:12:23 +0330

    Update Broadcast.lua

--- plugins/Broadcast.lua
+++ plugins/Broadcast.lua
@@ -1,12 +1,12 @@
 local function run(msg, matches)
 	if matches[1] == 'bc' and is_sudo(msg) then
-		tg.sendMessage(matches[2], 0, 0, matches[3], 0)
+		bot.sendMessage(matches[2], 0, 0, matches[3], 0)
 	end
 	if matches[1] == 'broadcast' and is_sudo(msg) then
 		local group = load_data(_config.group.data)
 		local text = matches[2]
 			for k,v in pairs(group[tostring('groups')]) do
-				tg.sendMessage(v, 0, 0, text, 0)
+				bot.sendMessage(v, 0, 0, text, 0)
 			end
 	end end return {
   patterns = {
