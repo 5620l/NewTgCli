00dcf4c3a3e246c4e0cc31a47590f5d35353fa92
p00ria <amir1300sobatdar@gmail.com>
2017-01-11 19:47:06 +0330

    Create config.lua

---
+++ bot/config.lua
@@ -0,0 +1,26 @@
+do local _ = {
+  enabled_plugins = {
+    "Plugins",
+    "GroupManager",
+    "AddRem",
+    "Msg_Checks",
+    "Admin",
+    "Pin",
+    "Broadcast",
+    "Tools",
+    "Moderator"
+  },
+  group  = {
+    data = "bot/group.json"
+  },
+  sudo_users = {
+    142141024,
+    198794027,
+  },
+robot = {
+    289372025,
+    0
+  }
+}
+return _
+end
