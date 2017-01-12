8e16d5d1f78d4da03c17acc47b26bcc0d825ce9e
p00ria <amir1300sobatdar@gmail.com>
2017-01-12 06:14:08 +0330

    Update Tools.lua

--- plugins/Tools.lua
+++ plugins/Tools.lua
@@ -6,10 +6,12 @@ end
 if matches[1] == 'creategroup' and is_sudo(msg) then
    bot.createNewGroupChat(matches[2], 1)
    bot.sendMessage(msg.chat_id_, 0, 1, '<b>Group '..matches[2]..' Created!</b>', 1, 'html')
-elseif matches[1] == 'upgrade' and is_sudo(msg) then
-   tg.migrateGroupChatToChannelChat(msg.chat_id_)
-elseif matches[1] == 'edit' and is_momod(msg) then
-   tg.editMessageText(msg.chat_id_, msg.reply_to_message_id_, nil, string.sub(matches[2], 7), 'html')
+end
+if matches[1] == 'upgrade' and is_sudo(msg) then
+   bot.migrateGroupChatToChannelChat(msg.chat_id_)
+end
+if matches[1] == 'edit' and is_momod(msg) then
+   bot.editMessageText(msg.chat_id_, msg.reply_to_message_id_, nil, matches[2] , 'html')
 end
 return {
    pattern = {
