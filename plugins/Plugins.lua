6a05f4656d09d2c59f73722ce9ce68c4a4433899
p00ria <amir1300sobatdar@gmail.com>
2017-01-13 15:49:56 +0330

    Update Plugins.lua

--- plugins/Plugins.lua
+++ plugins/Plugins.lua
@@ -1,6 +1,6 @@
 do
 
-local function plugin_enabled( name )
+local function plugin_enabled(name)
   for k,v in pairs(_config.enabled_plugins) do
     if name == v then
       return k
@@ -9,7 +9,7 @@ local function plugin_enabled( name )
   return false
 end
 
-local function plugin_exists( name )
+local function plugin_exists(name)
   for k,v in pairs(plugins_names()) do
     if name..'.lua' == v then
       return true
@@ -61,7 +61,7 @@ local function list_plugins(only_enabled)
   bot.sendMessage(msg.chat_id_, 0, 1, text, 1, 'html')
 end
 
-local function enable_plugin( plugin_name )
+local function enable_plugin(plugin_name)
   print('checking if '..plugin_name..' exists')
   if plugin_enabled(plugin_name) then
     text =  'پلاگین[ '..plugin_name..' ] فعال است'
@@ -78,7 +78,7 @@ local function enable_plugin( plugin_name )
 	bot.sendMessage(msg.chat_id_, 0, 1, text, 1, 'html')
   end
 end
-local function disable_plugin( name, chat )
+local function disable_plugin(name, chat)
   if not plugin_exists(name) then
     text = 'پلاگین [ '..name..' ]وجود ندارد'
 	bot.sendMessage(msg.chat_id_, 0, 1, text, 1, 'html')
@@ -100,19 +100,18 @@ local function run(msg, matches)
     return list_all_plugins()
   end
 
-  if matches[1] == '+' and is_sudo(msg) then
-    local plugin_name = matches[2]
-    print("enable: "..matches[2])
+  if matches[2] == '+' and is_sudo(msg) then
+    local plugin_name = matches[3]
+    print("enable: "..matches[3])
     return enable_plugin(plugin_name)
   end
 end
-  if matches[1] == '-' and is_sudo(msg) then 
-    if matches[2] == 'plugins' then
+  if matches[2] == '-' and is_sudo(msg) then 
+    if matches[3] == 'plugins' then
     	text = 'This plugin can\'t be disabled'
 		bot.sendMessage(msg.chat_id_, 0, 1, text, 1, 'html')
-    end
-    print("disable: "..matches[2])
-    return disable_plugin(matches[2])
+    print("disable: "..matches[3])
+    return disable_plugin(matches[3])
   end
   if matches[1] == 'reload' and is_sudo(msg) then --after changed to moderator mode, set only sudo
   text = 'بارگذاری مجدد انجام شد.'
@@ -134,5 +133,3 @@ return {
     },
   run = run,
 }
-
-end
